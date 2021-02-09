796604afb8cdb9d234b5843aea3e429acd0b69fe
Rahul Beniwal <rahulbeniwal26119@gmail.com>
2021-02-09 14:42:08 +0530

    file for fetching all data using axios

---
+++ src/API/index.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async () => {
+  try {
+    const {
+      data: {
+        confirmed,
+        recovered,
+        deaths,
+        lastUpdate
+      }
+    } = await axios.get(url);
+    const modifiedObject = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate
+    }
+
+    return modifiedObject
+  } catch (error) {}
+}
+
+export const fetchDailyData = async () => {
+  try {
+    const {data} = await axios.get(`${url}/daily`);
+    const modifiedData = data.map((dailyData)=>({
+      confirmed : dailyData.confirmed.total,
+      deaths : dailyData.deaths.total,
+      date : dailyData.reportDate,
+    }))
+    console.log(modifiedData)
+    return modifiedData;
+  } catch (error) {
+
+  }
+}
