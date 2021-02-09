f4078b5f8cb91feea12e2a2f2a0e0b7b50af75e1
Rahul Beniwal <rahulbeniwal26119@gmail.com>
2021-02-09 17:08:37 +0530

    adding covid image and functionaliy for individual country data

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 import {Cards , Chart , CountryPicker}  from './Component'
 import styles from './App.module.css';
-import {fetchData} from './API'
+import {fetchData} from './API';
+import coronaImage from './images/coronavirus.jpeg';
 
 class App extends React.Component {
   state = {
-    data : {}
+    data : {},
+    country : '',
   }
 
   async componentDidMount() {
@@ -14,12 +16,20 @@ class App extends React.Component {
     this.setState({
       data : fetchedData})
   }
+
+   handleCountryChange = async (country)=>{
+     const fetchedData = await fetchData(country)
+     this.setState({data : fetchedData, country})
+     console.log(this.state);
+  }
+
   render() {
-    const {data} = this.state;
+    const {data , country} = this.state;
     return (<div className={styles.container}>
+      <img className={styles.image} alt="COVID-19" src={coronaImage}/>
       <Cards data={data}/>
-    <Chart/>
-      <CountryPicker/>
+    <CountryPicker handleCountryChange = {this.handleCountryChange}/>
+  <Chart data={data} country={country}/>
     </div>)
   }
 }
