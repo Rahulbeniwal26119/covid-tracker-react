ab6ce810839617a59d9e392803a77ebdbee4c7f4
Rahul Beniwal <rahulbeniwal26119@gmail.com>
2021-02-09 17:09:53 +0530

    adding bar charts for countries other than global

--- src/Component/Chart/Chart.jsx
+++ src/Component/Chart/Chart.jsx
@@ -2,7 +2,14 @@ import React, {useState, useEffect} from 'react';
 import {fetchDailyData} from '../../API';
 import {Line, Bar} from 'react-chartjs-2';
 import styles from './Chart.module.css';
-const Chart = () => {
+const Chart = ({
+  data: {
+    confirmed,
+    recovered,
+    deaths
+  },
+  country
+}) => {
 
   const [dailyData, setDailyData] = useState([])
 
@@ -13,10 +20,8 @@ const Chart = () => {
     }
     fetchAPI();
 
-    // console.log(dailyData);
-    console.log("let me test");
     // fetchAPI();
-  } , [])
+  }, [])
   // const fetchAPI = async () => {
   //   setDailyData(await fetchDailyData());
   // }
@@ -42,11 +47,40 @@ const Chart = () => {
         ]
       }}/>)
     : null)
+    console.log(confirmed, recovered, deaths)
+
+  const barChart = (
+    confirmed
+    ? (<Bar data={{
+        labels: [
+          'Infected', 'Recovered', 'Deaths'
+        ],
+        datasets: [
+          {
+            label: 'People',
+            backgroundColor: [
+              'rgba(0, 0, 255, 0.5)', 'rgba(0, 255, 0, 0.5)', 'rgba(255, 0, 0, 0.5)'
+            ],
+            data: [confirmed.value, recovered.value, deaths.value]
+          }
+        ]
+      }} options={{
+        legend: {
+          display: false
+        },
+        title: {
+          display: true,
+          text: `Current state in ${country}`
+        }
+      }}/>)
+    : null)
 
   return (< div className = {
     styles.container
   } > {
-    LineChart
+    country
+      ? barChart
+      : LineChart
   } < /div>
   )
 }
