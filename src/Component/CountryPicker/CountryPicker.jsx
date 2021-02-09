e7f087ed4d80692a2c2efa9a8f83e218d0e07a72
Rahul Beniwal <rahulbeniwal26119@gmail.com>
2021-02-09 17:11:10 +0530

    adding functionlity for individual country and solving global return error

--- src/Component/CountryPicker/CountryPicker.jsx
+++ src/Component/CountryPicker/CountryPicker.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {NativeSelect, FormControl} from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import {fetchCountries} from '../../API';
 
-class CountryPicker extends React.Component {
-  render() {
-    return (<div>CountryPicker</div>)
-  }
+const CountryPicker = ({handleCountryChange}) => {
+  const [countriesFetched, setCountriesFetched] = useState([])
+
+  useEffect(() => {
+    const fetchedCountries = async () => {
+      setCountriesFetched(await fetchCountries())
+    }
+    fetchedCountries();
+  }, [setCountriesFetched])
+
+  console.log(countriesFetched)
+  return (<FormControl className={styles.formControl}>
+    <NativeSelect defaultValue="" onChange={(e)=>handleCountryChange(e.target.value)}>
+      <option value="">global</option>
+      {
+        countriesFetched.map((country, i) =>< option key = {
+          i
+        }
+        value = {
+          country
+        } > {
+          country
+        }</option>)
+      }
+    </NativeSelect>
+  </FormControl>)
 }
 
 export default CountryPicker;
